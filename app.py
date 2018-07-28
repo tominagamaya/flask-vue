c3053cce68d6fd740fbd94fba3b1a202440df0d7
tominagamaya <maya.tominaga.9@gmail.com>
2018-07-28 21:43:45 +0900

    追加機能を実装

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 DEBUG = True
@@ -15,13 +15,13 @@ BOOKS = [
         'read': True
     },
     {
-        'title': 'Harry Potter',
-        'author': 'J.K.Rowling',
-        'read': True
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
     },
     {
-        'title': 'Green Eggs',
-        'author': 'Dr.Seuss',
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
         'read': True
     }
 ]
@@ -32,12 +32,21 @@ def ping_pong():
     return jsonify('pong!')
 
 
-@app.route('/books', methods=['GET'])
+@app.route('/books', methods=['GET', 'POST'])
 def all_books():
-    return jsonify({
-        'status': 'success',
-        'books': BOOKS
-    })
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+
+    return jsonify(response_object)
 
 
 if __name__ == '__main__':
--- front/client/src/components/Books.vue
+++ front/client/src/components/Books.vue
@@ -4,17 +4,17 @@
       <div class="col-sm-10">
         <h1>Books</h1>
         <hr><br><br>
-        <button type="button" class="btn btn-success btn-sm">Add Book</button>
+        <button type="button" class="btn btn-success btn-sm" v-b-modal.book-modal>Add Book</button>
         <br><br>
         <table class="table table-hover">
-          <thread>
+          <thead>
             <tr>
               <th scope="col">Title</th>
               <th scope="col">Author</th>
               <th scope="col">Read?</th>
               <th></th>
             </tr>
-          </thread>
+          </thead>
           <tbody>
             <tr v-for="(book, index) in books" :key="index">
               <td>{{ book.title }}</td>
@@ -32,6 +32,40 @@
         </table>
       </div>
     </div>
+    <b-modal ref="addBookModal"
+             id="book-modal"
+             title="Add a new book"
+             hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+      <b-form-group id="form-title-group"
+                    label="Title:"
+                    label-for="form-title-input">
+          <b-form-input id="form-title-input"
+                        type="text"
+                        v-model="addBookForm.title"
+                        required
+                        placeholder="Enter title">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-author-group"
+                      label="Author:"
+                      label-for="form-author-input">
+            <b-form-input id="form-author-input"
+                          type="text"
+                          v-model="addBookForm.author"
+                          required
+                          placeholder="Enter author">
+            </b-form-input>
+          </b-form-group>
+        <b-form-group id="form-read-group">
+          <b-form-checkbox-group v-model="addBookForm.read" id="form-checks">
+            <b-form-checkbox value="true">Read?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
@@ -42,6 +76,11 @@ export default {
   data() {
     return {
       books: [],
+      addBookForm: {
+        title: '',
+        author: '',
+        read: [],
+      },
     };
   },
   methods: {
@@ -55,6 +94,41 @@ export default {
           // eslint-disable-next-line
           console.error(error);
         });
+    },
+    addBook(payload) {
+      const path = 'http://localhost:5000/books';
+      axios.post(path, payload)
+        .then(() => {
+          this.getBooks();
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.getBooks();
+        });
+    },
+    initForm() {
+      this.addBookForm.title = '';
+      this.addBookForm.author = '';
+      this.addBookForm.read = [];
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      let read = false;
+      if (this.addBookForm.read[0]) read = true;
+      const payload = {
+        title: this.addBookForm.title,
+        author: this.addBookForm.author,
+        read, // property shorthand
+      };
+      this.addBook(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      this.initForm();
     },
   },
   created() {
